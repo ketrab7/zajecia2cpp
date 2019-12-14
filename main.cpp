36962b30d8347add97a5966fa6a34032a45b3cd3
Bartek <b13chruslicki@gmail.com>
2019-12-14 09:48:15 +0100

    tworzymy klase

--- main.cpp
+++ main.cpp
@@ -1,21 +1,11 @@
+#include "tstring.h"
 #include <iostream>
-#include <random>
 #include <string>
-#include <ctime>
 
 using namespace std;
 
 
 
 int main () {
-   string s1 = "wlazl kotek na plotek i mruga";
-   for ( auto c: s1) cout <<c<<" ";
-   for ( auto& c : s1 ) c = (c=='w') ? 'W' : c;
-
-   for (int i=0; i <s1.length(); ++i) cout<<s1[i]<<" ";
-
-   string::iterator it;
-   it = s1.begin();
-
-   while ( it != s1.end()) { cout<<*it<<endl; ++it; }
+   TString s1;
 }
---
+++ tstring.h
@@ -0,0 +1,14 @@
+#ifndef TSTRING_H
+#define TSTRING_H
+#include <cstring>
+
+class TString {
+    public:
+
+    private:
+        char* ptr = nullptr;
+        std::size_t len = 0;
+    protected:
+
+};
+#endif
